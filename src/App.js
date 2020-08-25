b3553e3d7e5a57015613a5bc8ea4aeb222f2ede6
Ben Garlock <evilbunnyrabbits@gmail.com>
2020-08-25 14:38:22 -0400

    final commit before styling

--- src/App.js
+++ src/App.js
@@ -1,8 +1,45 @@
 import React from 'react';
 import './App.css';
+import { yes, no } from './objects'
 
-function App() {
-  return <h1>You Can Do This!</h1>;
+class App extends React.Component {
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      statementStatus: false,
+      imgUrl: no["no-image"],
+      statement: no["no-statment"]
+    }
+
+  }
+
+  toggleImage = () => {
+    if (this.state.statementStatus === false) {
+      this.setState({
+        statementStatus: true,
+        imgUrl: yes["yes-image"],
+        statement: yes["yes-statement"]
+
+      }, () => console.log(this.state.statementStatus))
+    } else {
+      this.setState({
+        statementStatus: false,
+        imgUrl: no["no-image"],
+        statement: no["no-statment"]
+      }, () => console.log(this.state.statementStatus, this.state.imgUrl))
+    }
+  }
+
+  render(){
+    return (
+        <div className="container">
+          <div className="statement" onClick={this.toggleImage}>{this.state.statement}</div>
+          <div className="image-container"><img src={this.state.imgUrl} /></div>
+        </div>
+    )
+  }
 }
 
 export default App;
