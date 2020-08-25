fa0e4eb97e78e1745a7170e107270466a6ef56ed
dukhyunko93 <dukhyunko93@gmail.com>
2020-08-25 14:42:57 -0400

    Done.

--- src/App.js
+++ src/App.js
@@ -1,8 +1,29 @@
 import React from 'react';
 import './App.css';
+import { yes, no } from './objects'
 
-function App() {
-  return <h1>You Can Do This!</h1>;
+class App extends React.Component {
+
+  state = {
+    status: false
+  }
+
+  handleClick = () => {
+    this.setState(previousStatus => {
+      return {
+        status: !previousStatus.status
+      }
+    }, () => console.log(this.state.status))
+  }
+
+  render(){
+    return (
+      <React.Fragment>
+        {this.state.status ? <h1>{yes["yes-statement"]}</h1> : <h1>{no["no-statment"]}</h1>}
+        {this.state.status ? <img src={yes["yes-image"]} onClick={this.handleClick} alt={this.status}/> : <img src={no["no-image"]} onClick={this.handleClick} alt={this.status}/>}
+      </React.Fragment>
+    )
+  }
 }
 
 export default App;
