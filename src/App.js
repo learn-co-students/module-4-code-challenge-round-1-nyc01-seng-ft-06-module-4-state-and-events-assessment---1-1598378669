5213e86a6729e4a737b4581199884d341434fd0a
DCR430 <daniel.c.reyes30@gmail.com>
2020-08-25 15:17:35 -0400

    danielR

--- src/App.js
+++ src/App.js
@@ -1,8 +1,32 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
+import {yes,no} from './objects.js'
 
-function App() {
-  return <h1>You Can Do This!</h1>;
+
+
+class App extends React.Component {
+
+  state = {
+    answer: "yes"
+  }
+
+  click = () => {
+    this.setState(currentState => {
+      return{
+        answer: !currentState.answer
+      }
+    }, 
+    () => console.log(this.state.answer))
+  }
+
+  render(){  
+    return (
+      <div className=".App">
+        {this.state.answer === "yes" ? <h1>{yes["yes-statement"]}</h1> : <h1>{no["no-statment"]}</h1>}
+        {this.state.answer === "yes" ? <img alt={yes["yes-statement"]} src={yes["yes-image"]} onClick={this.click} />: <img  alt={no["no-statement"]} src={no["no-image"]} onClick={this.click} />}
+      </div>
+    )
+  }
+
 }
-
 export default App;
