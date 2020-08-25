dd20a3e8e8b1ad2ee7457e43d4ac6731dc6e71f6
Dmitry <axiumone@gmail.com>
2020-08-25 14:50:17 -0400

    solution

--- src/App.js
+++ src/App.js
@@ -1,8 +1,26 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
+import { yes, no } from './objects.js'
 
-function App() {
-  return <h1>You Can Do This!</h1>;
+class App extends Component {
+
+  state = {clicked: false}
+
+  clickHandler=()=>{
+    this.setState({clicked: !this.state.clicked})
+  }
+
+  render() {
+    let statement = this.state.clicked ? yes['yes-statement'] : no['no-statment']
+    let image = this.state.clicked ? yes['yes-image'] : no['no-image']
+
+    return (
+      <div>
+        <h1 className="App-logo">{statement}</h1>
+        <img onClick={this.clickHandler}src={image}/>
+      </div>
+    )
+  }
 }
 
 export default App;
