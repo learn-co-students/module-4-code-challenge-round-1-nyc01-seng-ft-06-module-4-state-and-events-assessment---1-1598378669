8a8097c28373bc62434aff1c6546fdaa120e9dcd
Jeff-Adler <Jeffm.adler@gmail.com>
2020-08-25 14:25:18 -0400

    completed basic deliverables

--- src/App.js
+++ src/App.js
@@ -1,8 +1,32 @@
 import React from 'react';
 import './App.css';
+import {yes,no} from './objects.js'
 
-function App() {
-  return <h1>You Can Do This!</h1>;
+class App extends React.Component {
+
+  state = {
+    condition: "yes"
+  }
+
+  clickHandler = () => {
+    this.setState(previousState => {
+      if (previousState.condition === "yes") {
+        return {condition:"no"}
+      } else { 
+        return {condition: "yes"}
+      }
+    })
+  }
+
+  render(){  
+    return (
+      <div>
+        {this.state.condition === "yes" ? <h3>{yes["yes-statement"]}</h3> : <h3>{no["no-statement"]}</h3>}
+        {this.state.condition === "yes" ? <img alt={yes["yes-statement"]} src={yes["yes-image"]} onClick={this.clickHandler} />: <img alt={no["no-statement"]} src={no["no-image"]} onClick={this.clickHandler} />}
+      </div>
+    )
+  }
+
 }
 
 export default App;
