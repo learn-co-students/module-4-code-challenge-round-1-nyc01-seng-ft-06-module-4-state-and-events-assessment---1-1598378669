66dd82166c9b50206e38d036eafbb78319684513
RitaHartnell <mel.soriano6123@gmail.com>
2020-08-25 15:01:25 -0400

    deliverables done

--- src/App.js
+++ src/App.js
@@ -1,8 +1,41 @@
 import React from 'react';
 import './App.css';
+import {yes,no} from './objects'
 
-function App() {
-  return <h1>You Can Do This!</h1>;
+class App extends React.Component {
+  constructor() {
+    super()
+    this.state = {
+      statement: no["no-statment"],
+      picture: no["no-image"]
+    }
+  }
+
+  clickHandler = () => {
+    let which = this.state.statement
+    //console.log(which)
+    if (which === yes["yes-statement"]) {
+      which = {
+        statement: no["no-statment"],
+        picture: no["no-image"]
+      }
+    } else {
+      which = {
+        statement: yes["yes-statement"],
+        picture: yes["yes-image"]
+      }
+    }
+    this.setState({statement: which.statement, picture: which.picture})
+  }
+
+  render() {
+    return (
+      <div className='bigBox' onClick={this.clickHandler}>
+        <h2>{this.state.statement}</h2>
+        <img alt="" src={this.state.picture}></img>
+      </div>
+    )
+  }
 }
 
 export default App;
