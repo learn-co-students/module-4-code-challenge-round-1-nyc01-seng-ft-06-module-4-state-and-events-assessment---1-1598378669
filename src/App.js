6951ccb54f608d741fa3b8b44bd78c2c775722e5
roman9891 <roman9891@gmail.com>
2020-08-25 15:02:24 -0400

    deliverables and css

--- src/App.js
+++ src/App.js
@@ -1,8 +1,28 @@
 import React from 'react';
 import './App.css';
+import {yes, no} from './objects.js'
 
-function App() {
-  return <h1>You Can Do This!</h1>;
+class App extends React.Component {
+  state = {
+    yes: true
+  }
+  
+  clickHandler = () => {
+    let yesOrNo = !this.state.yes
+
+    this.setState({
+      yes: yesOrNo
+    })
+  }
+
+  render() {
+    return (
+      <div>
+        {this.state.yes ? <h1>Yes</h1> : <h1>No</h1>}
+        {this.state.yes ? <img src={yes['yes-image']} alt="yes" onClick={this.clickHandler}></img> : <img src={no['no-image']} alt="no" onClick={this.clickHandler}></img>}
+      </div>
+    )
+  }
 }
 
 export default App;
