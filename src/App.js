40beeaa278c0e1ec742fc1af3e9f0dbd5a33d912
catd825 <catd825@gmail.com>
2020-08-25 15:02:01 -0400

    finished

--- src/App.js
+++ src/App.js
@@ -1,8 +1,46 @@
 import React from 'react';
 import './App.css';
+import { yes, no } from './objects.js'
 
-function App() {
-  return <h1>You Can Do This!</h1>;
+class App extends React.Component {
+
+  state = {clicked: false}
+    //set a default state so we can update it later
+
+  clickHandler = () => {
+    this.setState(previousState => {
+      return {
+        clicked: !previousState.clicked, 
+      } 
+    })
+  }
+
+  //set clickHandler - pass through previous state, update the 'clicked' key with the opposite of its previous value
+
+
+  render(){
+  return <div>
+    <h1>You Can Do This!</h1>
+    <img src={this.state.clicked ? yes["yes-image"] : no["no-image"]} onClick = {this.clickHandler} />
+    <p>{this.state.clicked ? yes["yes-statement"] : no["no-statment"]}</p>
+    </div>;
+  }
 }
+//set image default, if clicked, invoke the click handler which switches the state
+
 
 export default App;
+
+
+/*Notes
+1. change to class since we are using an event listener
+2. default state should be not clicked
+
+*/
+
+
+{/* <img src={this.state.clicked ? yes["yes-image"] : no["no-image"]} onClick={this.clickHandler} /> */}
+
+// function App() {
+//   return <h1>You Can Do This!</h1>;
+// }
